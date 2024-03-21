1be44dcf5d45ae8df5c7880d3746e5178470a59d
Marius-P1 <marius.pain@epitech.eu>
2024-03-21 18:22:16 +0100

    ✨ Update Main module to import necessary functions and modules

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,47 @@
 
 module Main (main) where
 
-import Lib
+import Algorithm (
+        applyKMeans,
+        getFinalClusters
+    )
+
+import OurData (
+        Color(..),
+        Pixel(..)
+    )
+
+import ArgsParsing (
+        Options(..),
+        parseArgs,
+    )
+
+import FileParsing (
+        parseFile
+    )
+
+import Utils (
+        getRandomColor,
+        getFailingColor
+    )
+
+import System.Exit (exitWith, ExitCode(..))
+
+displayResult:: [(Color, [Pixel])] -> IO ()
+displayResult [] = return ()
+displayResult (a : as) = putStrLn "--" >>
+                        putStrLn (show (fst a)) >>
+                        putStrLn "-" >>
+                        mapM_ (putStrLn . show) (snd a) >>
+                        displayResult as
 
 main :: IO ()
-main = someFunc
+main = do
+    Options {oNumber = Just n, oLimit = Just l, oPath = Just p} <- parseArgs
+    rPixels <- parseFile p
+    if length rPixels < n
+        then putStrLn "Error: not enough pixels" >>
+            exitWith (ExitFailure 84)
+    else let randColors = getRandomColor rPixels n
+             colors = applyKMeans rPixels randColors (getFailingColor n) l
+         in displayResult (getFinalClusters rPixels colors colors)
